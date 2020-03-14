71f10b7d0708c96ced27336fe90e74d9971cd82e
Diana Wieczorek <wieczorek.diana@outlook.com>
2020-03-14 21:29:04 +0100

    validation test

--- app/scripts/task2.js
+++ app/scripts/task2.js
@@ -1,5 +1,29 @@
 window.onload = function () {
-  console.log("pipka")
+  document.getElementById("cardNumber").addEventListener("focusout", (e => {
+    console.log(validate({creditCardNumber: e.target.value}, constraints))
+  }));
+};
 
-
+const constraints = {
+  creditCardNumber: {
+    presence: true,
+    format: {
+      pattern: /^(34|37|4|5[1-5]).*$/,
+      message: function (value, attribute, validatorOptions, attributes, globalOptions) {
+        return validate.format("^%{num} is not a valid credit card number", {
+          num: value
+        });
+      }
+    },
+    length: function (value, attributes, attributeName, options, constraints) {
+      if (value) {
+        // Amex
+        if ((/^(34|37).*$/).test(value)) return {is: 15};
+        // Visa, Mastercard
+        if ((/^(4|5[1-5]).*$/).test(value)) return {is: 16};
+      }
+      // Unknown card, don't validate length
+      return false;
+    }
+  }
 }
